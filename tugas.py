15858e45cbafcc4c7dcb0d5158e628682ed9f21c
muhammad sultan <budionosultan8@gmail.com>
2025-10-31 08:05:14 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,21 @@
+barang1 = "roti"
+harga1 = 10000
+
+barang2 = "susu"
+harga2 = 3500
+
+barang3 = "gula"
+harga3 = 10500
+
+barang4 = "selai"
+harga4 = 12000
+
+barang5 = "garam"
+harga5 = 8000
+
+total_belanja = harga3 + harga1
+
+print("==== selamat datang di oktomart====")
+
+print("roti: Rp%s\nsusu: Rp%s\ngula: Rp%s\nselai: Rp%s\ngaram: Rp%s" %(harga1,harga2,harga3,harga4,harga5))
+print("total belanja: gula + roti = Rp%s" %(total_belanja))
---
+++ variable.py
@@ -0,0 +1,12 @@
+nama = "sultan"
+mapelfav = 'MTK'
+nilai = 1000
+perempuan = False
+
+print("======data nilai======")
+print('nama: %s' % (nama))
+print("mapelfav: %s\nnilai: %d\nperempuan: %r" %(mapelfav,nilai,perempuan))
+
+# %s = buat string
+# %d = numerik
+# %r = buat boolean
